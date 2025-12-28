a392134a966b6e577a7dd4a94938248945be07fc
gilgamesh7 <felidaesrule@yahoo.com>
2025-12-28 20:33:42 +1300

    create a transactional in memory DB , mcp server & first mcp tool

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-def main():
-    print("Hello from python-mcp-server!")
+import asyncio
 
+from mcp.server.fastmcp import FastMCP
+from transactional_db import CUSTOMERS_TABLE, ORDERS_TABLE, PRODUCTS_TABLE
 
-if __name__ == "__main__":
-    main()
+mcp = FastMCP("ecommerce_tools")
+
+@mcp.tool
+async def get_customer_info(customer_id: str) -> str:
+    """Search for a customer using their unique identifier"""
+
+    customer_info = CUSTOMERS_TABLE.get(customer_id, None)
+    if not customer_info:
+        return "Customer not found."
+    
+    return str(customer_info)
